0061d133da1ddf2f86275a933acfb0a856333e69
Pintenaru Melissa <melissa.pintenaru05@gmail.com>
2024-04-14 17:12:16 +0300

    add tests

--- todo/src/test/java/services/TaskServiceTest.java
+++ todo/src/test/java/services/TaskServiceTest.java
@@ -95,33 +95,30 @@ class TaskServiceTest {
 
     @Test
     void givenTooHighIndex_whenFindWhatTaskToDoNext_thenThrowException(){
-        List<Task> tasks = List.of(
-                new Task("Task 1", Priority.HIGH, 3),
-                new Task("Task 2", Priority.MEDIUM, 2),
-                new Task("Task 3", Priority.LOW, 1),
-                new Task("Task 4", Priority.HIGH, 5),
-                new Task("Task 5", Priority.MEDIUM, 4),
-                new Task("Task 6", Priority.LOW, 3),
-                new Task("Task 7", Priority.HIGH, 7),
-                new Task("Task 8", Priority.MEDIUM, 6),
-                new Task("Task 9", Priority.LOW, 5),
-                new Task("Task 10", Priority.HIGH, 9),
-                new Task("Task 11", Priority.MEDIUM, 8),
-                new Task("Task 12", Priority.LOW, 7),
-                new Task("Task 13", Priority.HIGH, 11),
-                new Task("Task 14", Priority.MEDIUM, 10),
-                new Task("Task 15", Priority.LOW, 9),
-                new Task("Task 16", Priority.HIGH, 13),
-                new Task("Task 17", Priority.MEDIUM, 12),
-                new Task("Task 18", Priority.LOW, 11),
-                new Task("Task 19", Priority.HIGH, 15),
-                new Task("Task 20", Priority.MEDIUM, 14),
-                new Task("Task 21", Priority.LOW, 13)
-        );
+        taskService.addTask(new Task("Task 1", Priority.HIGH, 3));
+        taskService.addTask(new Task("Task 2", Priority.MEDIUM, 2));
+        taskService.addTask(new Task("Task 3", Priority.LOW, 1));
+        taskService.addTask(new Task("Task 4", Priority.HIGH, 5));
+        taskService.addTask(new Task("Task 5", Priority.MEDIUM, 4));
+        taskService.addTask(new Task("Task 6", Priority.LOW, 3));
+        taskService.addTask(new Task("Task 7", Priority.HIGH, 7));
+        taskService.addTask(new Task("Task 8", Priority.MEDIUM, 6));
+        taskService.addTask(new Task("Task 9", Priority.LOW, 5));
+        taskService.addTask(new Task("Task 10", Priority.HIGH, 9));
+        taskService.addTask(new Task("Task 11", Priority.MEDIUM, 8));
+        taskService.addTask(new Task("Task 12", Priority.LOW, 7));
+        taskService.addTask(new Task("Task 13", Priority.HIGH, 11));
+        taskService.addTask(new Task("Task 15", Priority.LOW, 9));
+        taskService.addTask(new Task("Task 16", Priority.HIGH, 13));
+        taskService.addTask(new Task("Task 17", Priority.MEDIUM, 12));
+        taskService.addTask(new Task("Task 18", Priority.LOW, 11));
+        taskService.addTask(new Task("Task 19", Priority.HIGH, 15));
+        taskService.addTask(new Task("Task 20", Priority.MEDIUM, 14));
+        taskService.addTask(new Task("Task 21", Priority.LOW, 13));
 
-        Integer numberOfTasks = tasks.size(); // 21
+        Integer numberOfTasks = 21;
 
-        assertThrows(IllegalArgumentException.class, () -> taskService.findWhatTaskToDoNext(numberOfTasks, tasks));
+        assertThrows(IllegalArgumentException.class, () -> taskService.findWhatTaskToDoNext(numberOfTasks));
 
 
     }
